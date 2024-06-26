6bdbf86d5e3fa2f7e55e13b45b2e4f59da31f7e1
Keith Korecky <kkorecky@dragonspears.com>
2024-06-26 13:50:38 -0500

    Set up terraform configuration

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+    required_providers {
+        github = {
+            source = "integrations/github"
+            version = "4.6.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1" 
+    access_key = "AKIA2UC3FS2BBP6WVP3G"
+    secret_key = "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"
+}
+
+resource "aws_instance" "my_ec2" {
+    ami = "ami-0e001c9271cf7f3b9"
+    instance_type = "t2.micro"
+}
+
+provider "github" {
+    token = "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"
+}
+
+resource "github_repository" "terraform" {
+    name = "Terraform-Repo"
+    visibility = "public"
+}
+
+
